468bae66513a411800804115a02ae56262d436b6
beatricek222 <beatricek222@gmail.com>
2022-10-08 15:58:38 +0000

    Passed all each test

--- each_spell_word.rb
+++ each_spell_word.rb
@@ -1,6 +1,11 @@
 # Write a program that:
-# 
-# Asks your user to enter any word and have it spelled out letter by letter.  
+#
+# Asks your user to enter any word and have it spelled out letter by letter.
 
 p "Enter a word for me to spell:"
 
+letters = gets.chomp.split("")
+
+letters.each do |letter|
+  p letter.capitalize
+end
